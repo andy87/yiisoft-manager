c53bdf7a1cfd46ee757a96727835b0155b019c31
and_y87 <kidin_a@list.ru>
2019-10-31 16:24:08 +0300

    FIX

--- mapper.php
+++ mapper.php
@@ -3,19 +3,3 @@
 use \andy87\yii2\migrate\Manager;
 
 class_alias(Manager::class, 'andy87\yii2\migrate');
-
-
-$filename   = 'tpl/console_components_Manager.php';
-$filePath   = Yii::getAlias('@console/components/Manager.php');
-
-if ( !file_exists($filePath) )
-{
-    $dir = Yii::getAlias('@console/components');
-
-    if ( !is_dir($dir) )
-    {
-        mkdir($dir);
-    }
-
-    copy( $filename, $filePath );
-}
