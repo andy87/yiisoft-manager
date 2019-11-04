49491500fac6dfdcd0ab2ac09c0e07159e472579
and_y87 <kidin_a@list.ru>
2019-11-04 18:38:28 +0300

    Add TPL dir + files

---
+++ src/_tpl/console/components/Manager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace console\components;
+
+use andy87\yii2\manager\console\components\Manager as Source;
+
+/**
+ * Class Manager
+ *
+ *      Component
+ *
+ * @package console\components
+ */
+class Manager extends Source
+{
+    // ...
+}
